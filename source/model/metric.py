dbc3b49abe166b88131bab956b1310c7740fdced
Khanh-Toan-Nguyen <53964833+Khanh-Toan-Nguyen@users.noreply.github.com>
2020-12-30 22:07:52 +0700

    add metirc.py and utils.py
    add metirc.py and utils.py

---
+++ source/model/metric.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+# This is one of the metrics that can be used to understand the precision of the recommendation
+def r_precision(prediction, validation):
+    score = len(list(set(lst1) & set(lst2)))/len(validation)
+    return score
+
+### NDCG Code Source: https://gist.github.com/bwhite/3726239
+def dcg_at_k(r, k, method=0):
+    r = np.asfarray(r)[:k]
+    if r.size:
+        if method == 0:
+            return r[0] + np.sum(r[1:] / np.log2(np.arange(2, r.size + 1)))
+        elif method == 1:
+            return np.sum(r / np.log2(np.arange(2, r.size + 2)))
+        else:
+            raise ValueError('method must be 0 or 1.')
+    return 0.
+
+
+def ndcg_at_k(r, k, method=0):
+    dcg_max = dcg_at_k(sorted(r, reverse=True), k, method)
+    if not dcg_max:
+        return 0.
+    return dcg_at_k(r, k, method) / dcg_max
